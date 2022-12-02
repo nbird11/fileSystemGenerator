8694c3b9bad384fa044a4487c2d286b2c64e9ecc
Nathan Bird <bir19004@byui.edu>
2022-12-02 15:00:57 -0700

    initial commit

---
+++ create_file_system.py
@@ -0,0 +1,106 @@
+import os
+import customtkinter as ctk
+from customtkinter import filedialog
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("Create File System")
+        self.geometry(f"{720}x{480}")
+
+        self.label_root_dir = ctk.CTkLabel(self, text="Full path of root directory to create folder system in:")
+        self.label_root_dir.grid(column=0, row=0, columnspan=3, padx=10, pady=10, sticky=ctk.W)
+        self.entry_root_dir = ctk.CTkEntry(self, placeholder_text=os.path.abspath("."), width=500)
+        self.entry_root_dir.grid(column=0, row=1, columnspan=3, padx=10, sticky=ctk.W)
+        self.button_browse_local_dirs = ctk.CTkButton(self, text="Browse", command=self.browse_local)
+        self.button_browse_local_dirs.grid(column=4, row=1, padx=10, sticky=ctk.E)
+
+        self.label_root_name = ctk.CTkLabel(self, text="Name of the Root Folder for file structure:")
+        self.label_root_name.grid(column=0, row=2, padx=10, pady=10, sticky=ctk.W)
+        self.entry_root_name = ctk.CTkEntry(self, placeholder_text="Root", width=150)
+        self.entry_root_name.grid(column=0, row=3, padx=10, sticky=ctk.W)
+
+        self.label_num_units = ctk.CTkLabel(self, text="Number of units:")
+        self.label_num_units.grid(column=0, row=4, padx=10, pady=10, sticky=ctk.W)
+        self.entry_num_units = ctk.CTkEntry(self, width=30)
+        self.entry_num_units.grid(column=0, row=5, padx=10, sticky=ctk.W)
+
+        self.label_num_subunits = ctk.CTkLabel(self, text="Number of subunits:")
+        self.label_num_subunits.grid(column=1, row=4, padx=10, pady=10, sticky=ctk.W)
+        self.entry_num_subunits = ctk.CTkEntry(self, width=30)
+        self.entry_num_subunits.grid(column=1, row=5, padx=10, sticky=ctk.W)
+
+        self.label_unit_format = ctk.CTkLabel(self, text="Format for unit names:")
+        self.label_unit_format.grid(column=0, row=6, padx=10, pady=10, sticky=ctk.W)
+        self.entry_unit_format = ctk.CTkEntry(self, placeholder_text="Unit {0}", width=150)
+        self.entry_unit_format.grid(column=0, row=7, padx=10, sticky=ctk.W)
+        self.label_unit_format_tip = ctk.CTkLabel(self, text="({0} is the unit number)")
+        self.label_unit_format_tip.grid(column=0, row=8, padx=10, sticky=ctk.W)
+
+        self.label_subunit_format = ctk.CTkLabel(self, text="Format for subunit names:")
+        self.label_subunit_format.grid(column=1, row=6, padx=10, pady=10, sticky=ctk.W)
+        self.entry_subunit_format = ctk.CTkEntry(self, placeholder_text="Unit {0}.{1}", width=150)
+        self.entry_subunit_format.grid(column=1, row=7, padx=10, sticky=ctk.W)
+        self.label_subunit_format_tip = ctk.CTkLabel(self, text="({0} is the unit number;\n{1} is the subunit number)")
+        self.label_subunit_format_tip.grid(column=1, row=8, padx=10, sticky=ctk.W)
+
+
+        self.button_generate_dirs = ctk.CTkButton(self, text="Generate File System", command=self.generate_dirs)
+        self.button_generate_dirs.grid(column=4, row=10, padx=10, pady=100, sticky=ctk.E)
+
+
+
+    def browse_local(self):
+        dir_name = filedialog.askdirectory(initialdir=os.path.abspath("."))
+        self.entry_root_dir.configure(placeholder_text=os.path.abspath(dir_name))
+
+
+    def generate_dirs(self):
+        initialdir = os.path.abspath(".")
+
+        root_dir = self.entry_root_dir.get() if (self.entry_root_dir.get() != "") else self.entry_root_dir.cget("placeholder_text")
+        root_name = self.entry_root_name.get() if (self.entry_root_name.get() != "") else self.entry_root_name.cget("placeholder_text")
+
+        if self.entry_num_units.get() != "":
+            num_units = int(self.entry_num_units.get())
+        else:
+            raise AttributeError("Number of units has not been assigned.")      # Add pop-up window functionality
+        
+        if self.entry_num_subunits.get() != "":
+            num_subunits = int(self.entry_num_subunits.get())
+        else:
+            raise AttributeError("Number of subunits has not been assigned.")   # Add pop-up window functionality
+
+        unit_format = self.entry_unit_format.get() if (self.entry_unit_format.get() != "") else self.entry_unit_format.cget("placeholder_text")
+        subunit_format = self.entry_subunit_format.get() if (self.entry_subunit_format.get() != "") else self.entry_subunit_format.cget("placeholder_text")
+
+        os.chdir(root_dir)
+
+        os.mkdir(root_name)
+        os.chdir(root_name)
+        
+        for i in range(num_units):
+            dir_name = unit_format.format(i+1)
+            os.mkdir(dir_name)
+            os.chdir(dir_name)
+            for j in range(num_subunits):
+                sub_dir_name = subunit_format.format(i+1, j+1)
+                os.mkdir(sub_dir_name)
+            os.chdir("..")
+
+        os.chdir(initialdir)
+
+
+
+def main():
+    app = App()
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
